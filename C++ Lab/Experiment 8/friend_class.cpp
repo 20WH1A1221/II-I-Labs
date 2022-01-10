725f42af0b7def424f48793606eadf33dd0979c0
20WH1A1221 <93463149+20WH1A1221@users.noreply.github.com>
2022-01-10 19:59:06 +0530

    friend class

---
+++ C++ Lab/Experiment 8/friend_class.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class A {
+	private:
+		char a;
+		int num;
+	public:
+		friend class x;
+		
+};
+class x {
+	public:
+		void display(A b) {
+			cout << b.a << endl << b.num << endl;
+		}
+};
+int main() {
+	A a1;
+	x x1;
+	x1.display(a1);
+	return 0;
+}
